b8c8d3c79cebc7c2a3aa92a34df75f284d4a98d0
Jure Vito Srovin <jure.vito@gmail.com>
2022-04-15 16:52:20 +0200

    Start GPU implementation of histogram equalization

--- homework6/main.c
+++ homework6/main.c
@@ -15,6 +15,67 @@
 #include "stb_image_write.h"
 
 #define BINS 256
+#define MAX_SOURCE_SIZE 16384
+
+void cumulative_dist_gpu(histogram cum_dist, histogram hist) {
+
+    // Load kernel source file.
+    FILE* file;
+    file = fopen("hist_kernel.cl", "r");
+    if (!file) {
+        printf("Could not load the kernel.\n");
+        return;
+    }
+
+    char* source = (char*)malloc(MAX_SOURCE_SIZE);
+    size_t source_size = fread(source, 1, MAX_SOURCE_SIZE, file);
+    source[source_size] = '\0';
+    fclose(file);
+
+    // Boiler plate setup of platforms, devices, context and command queue.
+    cl_int cl_status;
+    cl_uint n_platforms;
+
+    cl_status = clGetPlatformIDs(0, NULL, &n_platforms);
+    cl_platform_id* platforms = (cl_platform_id*)malloc(sizeof(cl_platform_id) * n_platforms);
+    cl_status = clGetPlatformIDs(n_platforms, platforms, NULL);
+
+    cl_uint n_devices;
+    cl_status = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, 0, NULL, &n_devices);
+    n_devices = 1;
+
+    cl_device_id* devices = (cl_device_id*)malloc(sizeof(cl_device_id) * n_devices);
+    cl_status = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, n_devices, devices, NULL);
+
+    cl_context context = clCreateContext(NULL, n_devices, devices, NULL, NULL, &cl_status);
+    cl_command_queue cmd_queue = clCreateCommandQueue(context, devices[0], 0, &cl_status);
+
+    cl_program program = clCreateProgramWithSource(context, 1, (const char**)&source, NULL, &cl_status);
+    cl_status = clBuildProgram(program, 1, devices, NULL, NULL, NULL);
+
+    size_t log_size;
+	char* log;
+	cl_status = clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+    if (log_size > 2) {
+        log =(char *)malloc(sizeof(char)*(log_size+1));
+        cl_status = clGetProgramBuildInfo(program, devices[0], CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+        printf("%s", log);
+
+        free(log);
+        return 1;
+    }
+
+    // Divide work among the workgroups.
+
+    // Allocate memory on device.
+
+    // Create kernel and add arguments.
+
+    // Copy results from device back to host.
+
+    // Close resources and free memory.
+
+}
 
 void hist_equal_cpu(unsigned char* image, int width, int height, int cpp) {
 
@@ -65,6 +126,29 @@ void hist_equal_cpu(unsigned char* image, int width, int height, int cpp) {
     free(cum_dist.B);
 }
 
+void hist_equal_gpu(unsigned char* image, int width, int height, int cpp) {
+
+    // Create the histogram of the input image.
+    histogram hist;
+    hist.R = (unsigned int*)calloc(BINS, sizeof(unsigned int));
+    hist.G = (unsigned int*)calloc(BINS, sizeof(unsigned int));
+    hist.B = (unsigned int*)calloc(BINS, sizeof(unsigned int));
+
+    hist_gpu(image, hist, width, height, cpp);
+
+    // Calculate cumulative distribution.
+    histogram cum_dist; // Cumulative distribution.
+    cum_dist.R = (unsigned int*)calloc(BINS, sizeof(unsigned int));
+    cum_dist.G = (unsigned int*)calloc(BINS, sizeof(unsigned int));
+    cum_dist.B = (unsigned int*)calloc(BINS, sizeof(unsigned int));
+
+    cumulative_dist_gpu(cum_dist, hist);
+
+    // Calculate new color level values.
+
+    // Assign the new transformed value to each color channel of a pixel.
+}
+
 int main(int argc, char** argv) {
 
     if (argc < 2) {
@@ -77,7 +161,10 @@ int main(int argc, char** argv) {
     int width, height, cpp;
     unsigned char* image = stbi_load(img_file, &width, &height, &cpp, 0);
 
+    double start_time = omp_get_wtime();
     hist_equal_cpu(image, width, height, cpp);
+    double elapsed = omp_get_wtime() - start_time;
+    printf("CPU time: %.3lf\n", elapsed);
 
     // Save image after histogram equalization.
     char* output_name = malloc(4 + strlen(img_file) + 1);
